9d69e4a406f1930f47f1a2c5fdc2477e2b23c16d
theabsparrow <absbashar04@gmail.com>
2026-02-06 12:41:34 +0600

    analytics done

--- app/(commonLayout)/dashboard/page.tsx
+++ app/(commonLayout)/dashboard/page.tsx
@@ -1,16 +1,36 @@
 import DashboardOverview from "@/components/dashboard/customComponent/shared/DashboardOverview";
 import RevenueSection from "@/components/dashboard/customComponent/shared/RevenewSection";
 import Organization from "@/components/dashboard/organizations/Organization";
-import { getAnalytics, getUsageAnalytics } from "@/service/analytics";
+import {
+  getAnalytics,
+  getGrowthStats,
+  getRevenewStats,
+  getUsageAnalytics,
+} from "@/service/analytics";
+import { TSearchParams } from "@/types/shared";
 
-const HomePage = async () => {
-  const [analyticsResult, usageAnalyticsResult] = await Promise.all([
+const HomePage = async ({ searchParams }: { searchParams: TSearchParams }) => {
+  const query = await searchParams;
+
+  const [
+    analyticsResult,
+    usageAnalyticsResult,
+    useRevenewResult,
+    growthResult,
+  ] = await Promise.all([
     getAnalytics(),
     getUsageAnalytics(),
+    getRevenewStats(query),
+    getGrowthStats(query),
   ]);
-  const analytics = analyticsResult?.data || {};
-  const usageAnalytics = usageAnalyticsResult?.data;
 
+  const analytics = analyticsResult?.data || {};
+  const usageAnalytics = usageAnalyticsResult?.data || [];
+  const revenew = useRevenewResult?.data;
+  const growth = growthResult?.data;
+  console.log(revenew);
+  console.log(growth);
+  console.log(usageAnalytics);
   return (
     <div>
       <DashboardOverview analytics={analytics} />
